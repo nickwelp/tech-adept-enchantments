43560caa501a46da8c61dd5067826088b3e16ea3
nickwelp <nickwelp@gmail.com>
2022-01-11 23:20:43 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import enchantments from './enchantments';
+// {
+//   'weapon': {
+//      'Plasma Caliver': ['enchantments by description']
+//    }
+// }
+console.log(enchantments)
 function App() {
+  const [slot, setSlot] = useState('construct');
+  const [item, setItem] = useState();
+  const buildSlotLabels = () => {
+    return Object.keys(enchantments).map((slot) => {
+      return <label key={slot}>{slot}<input type="radio" name='slot' value={slot} onChange={(e)=> { setSlot(e.target.value); setItem('');}} /> | </label>
+    })
+  };
+  const buildItemLabels = () => {
+    // if(item) {
+      return Object.keys(enchantments[slot]).map((item) => {
+        return <label key={item}>{item}<input name='item' type="radio" value={item} onChange={(e)=> setItem(e.target.value)}/> | </label>
+      });
+    // }
+    // return null;
+  };
+  const showEnchantments = () => {
+    console.log(`show enchantments ${slot} ${item}`)
+    if(item) {
+      return enchantments[slot][item].map((desc) => {
+        return <li key={desc} dangerouslySetInnerHTML={{__html: desc}}></li>;
+      });
+    }
+    return null;
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This data was derived circa November 2021 and may be dated. This hopefully compliments the wonderful tool https://mome-borogove.github.io/40K-enchant-viewer/ with data for Tech Adepts  
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <p><strong>Pick a Slot</strong></p>
+      {buildSlotLabels()}
+      <p><strong>Pick an Item</strong></p>
+      {buildItemLabels()}
+      <ul>
+        {showEnchantments()}
+      </ul>
+
     </div>
   );
 }
